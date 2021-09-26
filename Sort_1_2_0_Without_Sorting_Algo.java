5a1005900cf5ac7c25849242a5f179bde1f6f5ee
Prajjwal Singh <Prajjwalsingh05032002@gmail.com>
2021-09-26 19:00:40 +0530

    Day 2

---
+++ Sort_1_2_0_Without_Sorting_Algo.java
@@ -0,0 +1,66 @@
+// Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+package Learning;
+
+import java.util.Scanner;
+
+class Sort_1_2_0_Without_Sorting_Algo {
+    void Display(int arr[],int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            System.out.printf("arr[%d]=%d \n ",i,arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Max_And_Min_Element_Array obj=new Max_And_Min_Element_Array();
+        int zero=0;
+        int first=0;
+        int two =0;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Number of Element to be present in the array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter the Element to be inserted:");
+            arr[i]=sc.nextInt();
+
+        }
+        obj.Display(arr,n);
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                zero=zero+1;
+            }
+            else if(arr[i]==1)
+            {
+                first=first+1;
+
+            }
+            else if(arr[i]==2)
+            {
+                two=two+1;
+            }
+
+        }
+        for(int i=0;i<zero;i++)
+        {
+            System.out.print("0,");
+        }
+        for(int i=0;i<first;i++)
+        {
+            System.out.print("1,");
+        }
+        for(int i=0;i<two;i++)
+        {
+            System.out.print("2,");
+        }
+
+
+
+    }
+}
